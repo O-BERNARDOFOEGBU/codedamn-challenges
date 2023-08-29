5eecb9e4f926105e8a3afb8e5085c206c400a40d
O-BERNARDOFOEGBU <obinnaofoegb71@gmail.com>
2023-08-29 18:05:51 +0000

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * Create and export getEmailCount function here
+ * The function should take a string and return count of valid emails
+ */
+
+export const getEmailCount = (inputString) => {
+    const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+    const matches = inputString.match(emailRegex);
+    if(!matches){
+        return 0
+    }
+    return matches.length;
+}
+
